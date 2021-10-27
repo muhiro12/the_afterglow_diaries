2fda91ffd4fb603f80a7024eb5a0d08dbba75c6c
muhiro_ <muhiro.12@gmail.com>
2021-10-27 16:57:23 +0900

    Add article page

---
+++ lib/article_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ArticlePage extends StatelessWidget {
+  const ArticlePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(toString()),
+      ),
+      body: Center(
+        child: Text(toString()),
+      ),
+    );
+  }
+}
--- lib/root_page.dart
+++ lib/root_page.dart
@@ -1,34 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:the_afterglow_diaries/article_page.dart';
 
 class RootPage extends HookWidget {
   const RootPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final counter = useState(0);
     return Scaffold(
       appBar: AppBar(
         title: const Text('The Afterglow Diaries'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '${counter.value}',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => counter.value++,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: GridView.count(
+        crossAxisCount: 4,
+        children: [
+          0,
+          1,
+          2,
+          3,
+          4,
+          5,
+        ]
+            .map(
+              (e) => Card(
+                child: MaterialButton(
+                  onPressed: () => Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const ArticlePage(),
+                    ),
+                  ),
+                  child: Text(
+                    e.toString(),
+                    style: Theme.of(context).textTheme.headline2,
+                  ),
+                ),
+              ),
+            )
+            .toList(),
       ),
     );
   }
