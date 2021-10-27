a26c2e2bc74e4436b31de5dd012ecd8a6a195456
muhiro_ <muhiro.12@gmail.com>
2021-10-27 15:37:12 +0900

    Add App file

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:the_afterglow_diaries/root_page.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const RootPage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:the_afterglow_diaries/root_page.dart';
+import 'package:the_afterglow_diaries/app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const RootPage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  runApp(const App());
 }
