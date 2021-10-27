a5242a6ee1df166c00b35cbb14344bb7c7ff3f57
muhiro_ <muhiro.12@gmail.com>
2021-10-27 15:34:14 +0900

    Add RootPage

---
+++ lib/root_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class RootPage extends StatefulWidget {
+  const RootPage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
