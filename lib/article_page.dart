042fdd04e6c18f40f2ead7a70fb1748fa5c2c9e0
muhiro_ <muhiro.12@gmail.com>
2021-10-28 09:15:25 +0900

    Read markdown from local file

--- lib/article_page.dart
+++ lib/article_page.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:the_afterglow_diaries/gen/assets.gen.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class ArticlePage extends StatelessWidget {
+class ArticlePage extends HookWidget {
   const ArticlePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final data = useFuture(rootBundle.loadString(Assets.md.test)).data ?? '';
     return Scaffold(
       appBar: AppBar(
         title: Text(toString()),
       ),
-      body: Center(
-        child: Text(toString()),
+      body: Markdown(
+        data: data,
       ),
     );
   }
