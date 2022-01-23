61903a1355fc84cac6c28cf8ac3455de4b23ce2e
muhiro_ <muhiro.12@gmail.com>
2022-01-24 08:45:55 +0900

    Add photos api

---
+++ lib/api_client.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class APIClient {
+  final client = http.Client();
+
+  Future<List<String>> getPhotoList() async {
+    final url = Uri.parse(
+        'https://theafterglowdiaries.microcms.io/api/v1/photo_list?limit=30');
+    final response = await client.get(url, headers: {
+      'X-MICROCMS-API-KEY': ''
+    });
+    final body = jsonDecode(response.body);
+    final contents = body['contents'];
+    final photoList = contents.map((e) => e['photo']);
+    final urlList = photoList.map((e) => e['url']).cast<String>().toList();
+    return urlList;
+  }
+}
--- lib/root_page.dart
+++ lib/root_page.dart
@@ -1,42 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:the_afterglow_diaries/article_page.dart';
+import 'package:the_afterglow_diaries/api_client.dart';
 
 class RootPage extends HookWidget {
   const RootPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final photos = useFuture(APIClient().getPhotoList()).data ?? [];
     return Scaffold(
       appBar: AppBar(
         title: const Text('The Afterglow Diaries'),
       ),
       body: GridView.count(
-        crossAxisCount: 4,
-        children: [
-          0,
-          1,
-          2,
-          3,
-          4,
-          5,
-        ]
-            .map(
-              (e) => Card(
-                child: MaterialButton(
-                  onPressed: () => Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => const ArticlePage(),
-                    ),
-                  ),
-                  child: Text(
-                    e.toString(),
-                    style: Theme.of(context).textTheme.headline2,
-                  ),
-                ),
-              ),
-            )
-            .toList(),
+        crossAxisCount: 2,
+        children: photos.map((e) => Image.network(e)).toList(),
       ),
     );
   }
