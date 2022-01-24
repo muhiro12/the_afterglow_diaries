203594e0d2357361022c8f254ada9c200ef5b9ef
muhiro_ <muhiro.12@gmail.com>
2022-01-24 09:50:51 +0900

    Add env file

--- lib/api_client.dart
+++ lib/api_client.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
 class APIClient {
@@ -9,7 +10,7 @@ class APIClient {
     final url = Uri.parse(
         'https://theafterglowdiaries.microcms.io/api/v1/photo_list?limit=30');
     final response = await client.get(url, headers: {
-      'X-MICROCMS-API-KEY': ''
+      'X-MICROCMS-API-KEY': dotenv.get('X-MICROCMS-API-KEY'),
     });
     final body = jsonDecode(response.body);
     final contents = body['contents'];
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:the_afterglow_diaries/app.dart';
 
-void main() {
+void main() async {
+  await dotenv.load();
   runApp(const App());
 }
