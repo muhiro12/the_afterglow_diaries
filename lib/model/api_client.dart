26ecf1680ba1b7bce5e0bcef3944fd97f653cbd7
muhiro_ <muhiro.12@gmail.com>
2022-02-28 21:10:30 +0900

    Add apiClientProvider

--- lib/model/api_client.dart
+++ lib/model/api_client.dart
@@ -1,22 +1,4 @@
-import 'dart:convert';
-
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:http/http.dart' as http;
-import 'package:the_afterglow_diaries/entity/photo_list_response.dart';
 
-class APIClient {
-  final client = http.Client();
-
-  Future<List<String>> getPhotoList() async {
-    final url = Uri.parse(
-        'https://theafterglowdiaries.microcms.io/api/v1/photo_list?limit=30');
-    final response = await client.get(url, headers: {
-      'X-MICROCMS-API-KEY': dotenv.get('X-MICROCMS-API-KEY'),
-    });
-    final json = jsonDecode(response.body);
-    return PhotoListResponse.fromJson(json)
-        .contents
-        .map((e) => e.photo.url)
-        .toList();
-  }
-}
+final apiClientProvider = Provider((ref) => http.Client());
--- lib/model/photo_controller.dart
+++ lib/model/photo_controller.dart
@@ -1,6 +1,23 @@
+import 'dart:convert';
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:the_afterglow_diaries/entity/photo_list_response.dart';
 import 'package:the_afterglow_diaries/model/api_client.dart';
 
 final photoListProvider = FutureProvider(
-  (ref) => APIClient().getPhotoList(),
+  (ref) async {
+    final url = Uri.parse(
+        'https://theafterglowdiaries.microcms.io/api/v1/photo_list?limit=30');
+    final response = await ref.watch(apiClientProvider).get(
+      url,
+      headers: {
+        'X-MICROCMS-API-KEY': dotenv.get('X-MICROCMS-API-KEY'),
+      },
+    );
+    return PhotoListResponse.fromJson(json.decode(response.body))
+        .contents
+        .map((e) => e.photo.url)
+        .toList();
+  },
 );
